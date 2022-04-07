4dbdf766271575619c5c62c541e0191af733cb6a
zabec <denis.imsirovic@gmail.com>
2022-04-07 12:57:29 +0200

    ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,26 @@
+
+var temps = [[31, 32, 19, 37],
+             [29, 27, 55, 36],
+             [17, 27, 42, 46],
+             [29, 52, 21, 64],
+             [91, 27, 31, 61]];
+
+// Only change code below this line
+function myArrayFunction() {
+  var averageDayTemp = [];
+  for(var i = 0; i < temps.length; i++) {
+    var sum = 0;
+    var count = 0;
+    for(var j = 0; j < temps[i].length; j++) {
+      sum += temps[i][j];
+      count += 1;
+    }
+    averageDayTemp.push(sum / count);
+  }
+  return averageDayTemp;
+}
+
+console.log(myArrayFunction());
+// Only change code above this line
+module.exports = myArrayFunction;
+
