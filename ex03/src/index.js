85a871d086b1674b51f8c2c0d51660d0a9d5e872
zabec <denis.imsirovic@gmail.com>
2022-04-07 12:43:16 +0200

    ex03

---
+++ ex03/src/index.js
@@ -0,0 +1,17 @@
+
+// Only change code below this line
+var myPetsArray = ["Dog", "Cat"]; 
+
+function myArrayFunction(myPets) {
+  var myNewPets = myPets;
+  myNewPets.push(...["Bird", "Fish"]);
+  var firstPet = myNewPets.shift();
+  var lastPet = myNewPets.pop();
+  myNewPets.unshift("Lion");
+  return myNewPets;
+}
+
+console.log(myArrayFunction(myPetsArray));
+// Only change code above this line
+module.exports = myArrayFunction;
+
